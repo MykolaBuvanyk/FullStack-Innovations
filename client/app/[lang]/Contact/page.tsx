2fa335ab940d9c2282a51daa83a7c5457ca6d501
unknown <a.o.borzenko@student.khai.edu>
2025-03-27 19:50:54 +0200

    add contact component

---
+++ client/app/[lang]/Contact/page.tsx
@@ -0,0 +1,10 @@
+// import styles from "./page.module.css";
+import ContactContent from "../../components/ContactContent/ContactContent"
+
+export default function Home() {
+  return (
+    <div>
+      <ContactContent/>
+    </div>
+  );
+}
