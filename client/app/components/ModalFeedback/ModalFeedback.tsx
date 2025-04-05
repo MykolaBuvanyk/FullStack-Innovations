91d31b22f277a50fc3e62e5240076389d76ef24d
unknown <a.o.borzenko@student.khai.edu>
2025-04-05 19:57:19 +0300

    fix modalfeedback

---
+++ client/app/components/ModalFeedback/ModalFeedback.tsx
@@ -0,0 +1,234 @@
+"use client";
+
+import { useState } from "react";
+import styles from "./ModalFeedback.module.css";
+
+const ModalFeedback: React.FC<ModalFeedbackProps> = ({
+  isOpen,
+  closeModal,
+}) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    feedbackType: "1",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    message: "",
+    feedbackType: "",
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [focused, setFocused] = useState({ feedbackType: true });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    setErrors((prev) => ({ ...prev, [name]: "" }));
+  };
+
+  const validateForm = () => {
+    const newErrors = {
+      name: "",
+      email: "",
+      message: "",
+      feedbackType: "",
+    };
+    let isValid = true;
+
+    if (!formData.name.trim()) {
+      newErrors.name = "Заповніть ім'я";
+      isValid = false;
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Заповніть пошту";
+      isValid = false;
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "Введіть коректний E-mail";
+      isValid = false;
+    }
+
+    if (!formData.feedbackType) {
+      newErrors.feedbackType = "Виберіть оцінку";
+      isValid = false;
+    }
+
+    if (!formData.message.trim()) {
+      newErrors.message = "Заповніть відгук або пропозицію";
+      isValid = false;
+    }
+    
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
+
+    console.log("Форма відправлена:", formData);
+    setIsSubmitted(true);
+
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+      feedbackType: "1",
+    });
+
+    setTimeout(() => {
+      setIsSubmitted(false);
+      closeModal();
+    }, 5000);
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalFeedbackWrapper}>
+        <button className={styles.backButton} onClick={closeModal}>
+          <i className="fa-solid fa-arrow-left"></i>
+        </button>
+        <div className={styles.titleWrapper}>
+          <h2 className={styles.title}>Відгук або пропозиція щодо роботи</h2>
+          <p className={styles.titlePara}>Нам важлива, ваша думка</p>
+        </div>
+        <form className={styles.form} onSubmit={handleSubmit} noValidate>
+          <div className={styles.formGroupWrapper}>
+            <div className={styles.formGroup}>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder=" "
+                className={styles.input}
+                value={formData.name}
+                onChange={handleChange}
+              />
+              <label htmlFor="name" className={styles.label}>
+                {/* {dictionary.fields.name} */}
+                Імя
+              </label>
+              {errors.name && (
+                <div className={styles.error}>{errors.name}</div>
+              )}
+            </div>
+            <div className={styles.formGroup}>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder=" "
+                className={styles.input}
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <label htmlFor="email" className={styles.label}>
+                {/* {dictionary.fields.email} */}
+                Емейл
+              </label>
+              {errors.email && (
+                <div className={styles.error}>{errors.email}</div>
+              )}
+            </div>
+            <div className={styles.formGroup}>
+              <input
+                type="text"
+                id="message"
+                name="message"
+                placeholder=" "
+                className={styles.input}
+                value={formData.message}
+                onChange={handleChange}
+              />
+              <label htmlFor="message" className={styles.label}>
+                Відгук або пропозиція
+              </label>
+              {errors.message && (
+                <div className={styles.error}>{errors.message}</div>
+              )}
+            </div>
+          </div>
+          <div className={styles.inputWrapper}>
+            <p
+              className={`${styles.dropDownPara} ${
+                focused.feedbackType
+                  ? styles.dropDownOpenPara
+                  : styles.dropDownClosedPara
+              }`}
+              onClick={() =>
+                setFocused((prev) => ({
+                  ...prev,
+                  feedbackType: !prev.feedbackType,
+                }))
+              }
+            >
+              Ваша оцінка
+              <i className="fa-solid fa-chevron-down"></i>
+            </p>
+
+            <div
+              className={`${styles.customSelect} ${
+                focused.feedbackType ? styles.selectFocused : ""
+              } ${
+                focused.feedbackType
+                  ? styles.dropdownOpen
+                  : styles.dropdownClosed
+              }`}
+            >
+              {focused.feedbackType && (
+                <div className={styles.options}>
+                  {["1", "2", "3", "4", "5"].map((val, idx) => {
+                    const labels = [
+                      "1 — Погано",
+                      "2 — Можливо краще",
+                      "3 — Терпимо",
+                      "4 — Добре",
+                      "5 — Відмінно",
+                    ];
+                    return (
+                      <div
+                        key={val}
+                        className={`${styles.option} ${
+                          formData.feedbackType === val ? styles.active : ""
+                        }`}
+                        onClick={() => {
+                          setFormData((prev) => ({
+                            ...prev,
+                            feedbackType: val,
+                          }));
+                          setErrors((prev) => ({ ...prev, feedbackType: "" }));
+                        }}
+                      >
+                        {labels[idx]}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+
+            {errors.feedbackType && (
+              <div className={styles.error}>{errors.feedbackType}</div>
+            )}
+          </div>
+          <button type="submit" className={styles.submitBtn}>
+            Відправити
+            <img src="/images/arrow_top_right.svg" alt="" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalFeedback;
--- client/app/components/TellAboutUs/TellAboutUs.tsx
+++ client/app/components/TellAboutUs/TellAboutUs.tsx
@@ -1,112 +1,139 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './TellAboutUs.module.css';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
-
-type Props = {
-  dictionary: any;
-};
+import ModalFeedback from '../ModalFeedback/ModalFeedback';
 
 const reviews = [
-  {
-    logo: '/images/logo.svg', 
-  },
-  {
-    logo: '/images/logo.svg',
-  },
-  {
-    logo: '/images/logo.svg',
-  },
-  {
-    logo: '/images/logo.svg',
-  },
+    {
+        logo: '/images/logo.png',
+        rating: 4,
+        text: 'With Codeks support, the client\'s website was functioning perfectly, and customers could easily place orders on the platform. Codeks effectively and promptly corrected the mistakes of previous developers. The service provider\'s professionalism and client-oriented approach were exemplary.',
+        name: 'Діана Кравець',
+        position: 'CEO',
+        date: '15.08.2023',
+    },
+    {
+        logo: '/images/logo.png',
+        rating: 5,
+        text: 'Codeks delivered an outstanding product that exceeded our expectations. Their attention to detail and commitment to quality made the entire process smooth and efficient. We highly recommend their services!',
+        name: 'Олег Павленко',
+        position: 'CTO',
+        date: '20.09.2023',
+    },
+    {
+        logo: '/images/logo.png',
+        rating: 3,
+        text: 'Working with Codeks was a game-changer for our business. They provided innovative solutions and excellent support throughout the project. We look forward to collaborating with them again!',
+        name: 'Аліна Мельник',
+        position: 'Project Manager',
+        date: '10.10.2023',
+    },
+    {
+        logo: '/images/logo.png',
+        rating: 3,
+        text: 'Working with Codeks was a game-changer for our business. They provided innovative solutions and excellent support throughout the project. We look forward to collaborating with them again!',
+        name: 'Аліна Мельник',
+        position: 'Project Manager',
+        date: '10.10.2023',
+    },
 ];
 
-const TellAboutUs: React.FC<Props> = ({ dictionary }) => {
-  const renderStars = (rating: number) => {
-    const fullStars = rating;
-    const emptyStars = 5 - fullStars;
+const TellAboutUs = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const renderStars = (rating) => {
+        const fullStars = rating;
+        const emptyStars = 5 - fullStars;
+
+        return (
+            <div className={styles.ItemPointStar}>
+                {[...Array(fullStars)].map((_, index) => (
+                    <div key={`full-${index}`} className={styles.ItemPointStarSolid}>
+                        <i className="fa-solid fa-star"></i>
+                    </div>
+                ))}
+                {[...Array(emptyStars)].map((_, index) => (
+                    <div key={`empty-${index}`} className={styles.ItemPointStarRegular}>
+                        <i className="fa-regular fa-star"></i>
+                    </div>
+                ))}
+            </div>
+        );
+    };
 
     return (
-      <div className={styles.ItemPointStar}>
-        {[...Array(fullStars)].map((_, index) => (
-          <div key={`full-${index}`} className={styles.ItemPointStarSolid}>
-            <i className="fa-solid fa-star"></i>
-          </div>
-        ))}
-        {[...Array(emptyStars)].map((_, index) => (
-          <div key={`empty-${index}`} className={styles.ItemPointStarRegular}>
-            <i className="fa-regular fa-star"></i>
-          </div>
-        ))}
-      </div>
+        <section className={styles.tellAboutUs}>
+            <div className={styles.tellAboutUsTitle}>
+                <div className={styles.tellAboutUsTitleButton}>
+                    <h2>
+                        Що кажуть<br />клієнти про нас
+                    </h2>
+                    <button className={styles.submitButton} onClick={openModal}>
+                        Залишити відгук
+                        <img src="/images/arrow_top_right.svg" alt="" />
+                    </button>
+                </div>
+                <p>
+                    Ми розробляємо, створюємо та супроводжуємо it-продукти з початку 2012 року. Ми можемо скористатися послугами нового циклу - від планування та ідей до запуску продукту.
+                </p>
+            </div>
+            <div className={styles.tellAboutUsSlider}>
+                <Swiper
+                    modules={[Pagination]}
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    pagination={{
+                        clickable: true,
+                        el: `.${styles.swiperPagination}`,
+                    }}
+                    breakpoints={{
+                        1100: { slidesPerView: 3 },
+                        768: { slidesPerView: 3 },
+                        0: { slidesPerView: 1 },
+                    }}
+                >
+                    {reviews.map((review, index) => (
+                        <SwiperSlide key={index}>
+                            <div className={styles.tellAboutUsSliderItem}>
+                                <div className={styles.tellAboutUsSliderItemFirst}>
+                                    <div className={styles.ItemLogo}>
+                                        <img src={review.logo} alt="Company Logo" />
+                                    </div>
+                                    <div className={styles.ItemPoint}>
+                                        <p>Оцінка:</p>
+                                        {renderStars(review.rating)}
+                                    </div>
+                                </div>
+                                <div className={styles.tellAboutUsSliderItemSecond}>
+                                    <p>{review.text}</p>
+                                </div>
+                                <div className={styles.tellAboutUsSliderItemThird}>
+                                    <div className={styles.ItemPersonName}>
+                                        <h4>{review.name}</h4>
+                                        <p>{review.position}</p>
+                                    </div>
+                                    <div className={styles.ItemDate}>
+                                        <p>{review.date}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className={styles.swiperPagination}></div>
+            </div>
+            <ModalFeedback isOpen={isModalOpen} closeModal={closeModal} />
+        </section>
     );
-  };
-
-  return (
-    <section className={styles.tellAboutUs}>
-      <div className={styles.tellAboutUsTitle}>
-        <div className={styles.tellAboutUsTitleButton}>
-          <h2 dangerouslySetInnerHTML={{ __html: dictionary.title }}></h2>
-          <button className={styles.submitButton}>
-            {dictionary.buttonText}
-            <img src="/images/arrow_top_right.svg" alt="" />
-          </button>
-        </div>
-        <p>{dictionary.description}</p>
-      </div>
-      <div className={styles.tellAboutUsSlider}>
-        <Swiper
-          modules={[Pagination]}
-          spaceBetween={0}
-          slidesPerView={1}
-          pagination={{
-            clickable: true,
-            el: `.${styles.swiperPagination}`,
-          }}
-          breakpoints={{
-            1100: { slidesPerView: 3 },
-            768: { slidesPerView: 3 },
-            0: { slidesPerView: 1 },
-          }}
-        >
-          {reviews.map((review, index) => (
-            <SwiperSlide key={index}>
-              <div className={styles.tellAboutUsSliderItem}>
-                <div className={styles.tellAboutUsSliderItemFirst}>
-                  <div className={styles.ItemLogo}>
-                    <img src={review.logo} alt="Company Logo" />
-                  </div>
-                  <div className={styles.ItemPoint}>
-                    <p>Оцінка:</p>
-                    {renderStars(dictionary.reviews[index].rating)}
-                  </div>
-                </div>
-                <div className={styles.tellAboutUsSliderItemSecond}>
-                  <p>{dictionary.reviews[index].text}</p>
-                </div>
-                <div className={styles.tellAboutUsSliderItemThird}>
-                  <div className={styles.ItemPersonName}>
-                    <h4>{dictionary.reviews[index].name}</h4>
-                    <p>{dictionary.reviews[index].position}</p>
-                  </div>
-                  <div className={styles.ItemDate}>
-                    <p>{dictionary.reviews[index].date}</p>
-                  </div>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <div className={styles.swiperPagination}></div>
-      </div>
-    </section>
-  );
 };
 
 export default TellAboutUs;
